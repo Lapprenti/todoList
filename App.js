bd9dac7019c328dc99ad1ae3e2bd7cb1e0c7e84d
arnaud.lapprand <arnaud.lapprand@diiage.org>
2021-04-01 12:30:50 +0200

    feat(task): added task section and task component

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Task from './components/Tasks';
 
+const data = [
+  {
+    id: 1,
+    thumbnail: '',
+    label: '🏋 Work out 30 minutes',
+    isCompleted: false
+  },
+  {
+    id: 2,
+    thumbnail: '',
+    label: '🏫 Ace the react native test',
+    isCompleted: false
+  },
+  {
+    id: 3,
+    thumbnail: '',
+    label: ' 🍸 Chill out',
+    isCompleted: false
+  },
+]
+
+{/* Todo today section */}
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Todo today section */}
+      <View style={styles.tasksSection}>
+        <Text style={styles.titleStyle}> Todo today </Text>
+        {
+          data.map(d => {
+            return(
+              <Task initialParams={{d}}></Task>
+            )
+          })
+        }
+        
+      </View>
     </View>
   );
 }
@@ -14,8 +48,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#8C82FF",
   },
+  tasksSection: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  titleStyle: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+    marginBottom: 50
+  }
 });
---
+++ components/Tasks.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Task = (props) => {
+  const task = props.initialParams.d;
+  return (
+    <View style={styles.item} key={task}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{task.label}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    marginVertical: 10,
+    backgroundColor: '#FFFFFF',
+    borderColor: '#F7F7F7',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 55,
+    justifyContent: 'center',
+    
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: 'rgba(47, 105, 255, 0.4)',
+    borderRadius: 5
+  },
+  itemText: {
+    minWidth: "80%",
+    paddingHorizontal: 50
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#2F69FF'
+  },
+});
+
+export default Task;
